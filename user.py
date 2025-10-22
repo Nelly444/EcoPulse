5353a3139806d856abf730da7d81ea88fbd1fd0b
Nelly444 <nelsonsupriyasilp111@gmail.com>
2025-10-21 17:19:18 -0700

    Add UserInterface class for Streamlit UI

--- user.py
+++ user.py
@@ -1,4 +1,27 @@
 #User Interface
 
-def main():
-    pass
+import streamlit as st
+
+import pandas as pd
+from datetime import datetime
+
+CSV_FILE = "waste_log.csv"
+
+
+class UserInterface:
+
+    def __init__(self):
+        self.st = st
+
+    def display_title(self):
+        self.st.title("Waste Management Tracker")
+    
+    def get_input(self):
+        pass
+        
+
+
+
+
+
+
